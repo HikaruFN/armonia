7c0ad92f1fcfa59b6b8aa79eb6bbd448288a6864
HikaruFN <francesconisi95@gmail.com>
2021-11-01 16:39:06 +0100

    update  filter method to delete elements

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,25 +1,25 @@
 import './Sidebar.css';
 function Sidebar(){
-    return (
-        <div className="sidebar-container">
-            <div className="sidebar-header">
-            <h2>Componenti</h2>
-                <ul>
-                    <li>
-                        <a href="http://localhost:3000/">Modulo di contatto</a>
-                    </li>
-                    <li>
-                        <a href="http://localhost:3000/">Chat</a>
-                    </li>
-                    <li>
-                        <a href="http://localhost:3000/">Stylish table</a>
-                    </li>
-                </ul>
-            </div>
-            <div className="sidebar-footer">
-                <h5>&lt;/&gt; by Pasquale</h5>
-            </div>    
-        </div>
-    );
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar-header">
+        <h2>Componenti</h2>
+          <ul>
+            <li>
+              <a href="http://localhost:3000/">Modulo di contatto</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000/">Chat</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000/">Stylish table</a>
+            </li>
+          </ul>
+      </div>
+      <div className="sidebar-footer">
+        <h5>&lt;/&gt; by Pasquale</h5>
+      </div>    
+    </div>
+  );
 }
 export default Sidebar;
--- src/components/StylishTable.js
+++ src/components/StylishTable.js
@@ -2,60 +2,56 @@ import './StylishTable.css';
 import React , {useState} from 'react';
 import axios from 'axios';
 function StylishTable(){
-    const [users, setUsers] = useState({
-        responseArr : []
+  const [users, setUsers] = useState({
+    responseArr : []
+  });
+  function callData(){
+    axios.get('https://ae5a5c88-dfd5-42e0-ab56-d2de610d754b.mock.pstmn.io//postman')
+    .then(response=>{
+      setUsers({
+        responseArr : response.data
+      })
     });
-    function callData(){
-        axios.get('https://ae5a5c88-dfd5-42e0-ab56-d2de610d754b.mock.pstmn.io//postman')
-        .then(response=>{
-            setUsers({
-                responseArr : response.data
-            })
-        });
-    };
-    function handleDelete(index){
-        let updatedResponseArr = [];
-        for(let i = 0; i < users.responseArr.length; i++){
-            if(i !== index){
-                updatedResponseArr.push(users.responseArr[i]);
-            }
-        }
-        setUsers({
-            responseArr : updatedResponseArr
-        })
-    }
-    return(
-        <div className='stylish-table-container'>
-            <div className='stylish-table-header'>
-                <h1># Stylish table</h1>
-                <button onClick={callData}>Ricarica dati</button>
-            </div>
-            <div className='table-container'>
-                <div className='table-data'>
-                    <span>Cliente</span>
-                    <span>Età</span>
-                    <span>Posizione</span>
-                    <span>Azioni</span>
-                </div>
-                <div className='table-cards'>
-                    {
-                        users.responseArr.map((item, index)=>{
-                            return(
-                                <div key={index} className='card'>
-                                    <div>
-                                        <img src={item.img} alt={item.name}/>
-                                        <span>{item.name}</span>
-                                    </div>
-                                    <span>{item.eta}</span>
-                                    <span>{item.job}</span>
-                                    <a onClick={()=>handleDelete(index)}>Cancella</a>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </div>      
+  };
+  function handleDelete(index){
+    const updateArray = users.responseArr.filter((item, i) => i !== index);
+    setUsers({
+      responseArr : updateArray
+    })
+  }
+  return(
+    <div className='stylish-table-container'>
+      <div className='stylish-table-header'>
+        <h1># Stylish table</h1>
+        <button onClick={callData}>Ricarica dati</button>
+      </div>
+      <div className='table-container'>
+        <div className='table-data'>
+          <span>Cliente</span>
+          <span>Età</span>
+          <span>Posizione</span>
+            <span>Azioni</span>
         </div>
-    );
+        <div className='table-cards'>
+          {
+            users.responseArr.map((item, index)=>{
+              return(
+                <div key={index} className='card'>
+                  <div>
+                    <img src={item.img} alt={item.name}/>
+                    <span>{item.name}</span>
+                  </div>
+                  <span>{item.eta}</span>
+                  <span>{item.job}</span>
+                  <a onClick={()=>handleDelete(index)}>Cancella</a>
+                </div>
+             )
+            })
+          }
+        </div>
+      </div>      
+    </div>
+  );
 }
+
 export default StylishTable;
