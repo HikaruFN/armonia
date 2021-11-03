d7b9a36a67e1167248bd42600f3b2937d50b0e26
HikaruFN <francesconisi95@gmail.com>
2021-11-03 12:39:14 +0100

    update sidebar.js comments

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,8 @@ function Sidebar() {
     <div className="sidebar-container">
       <div className="sidebar-header">
         <h2>Componenti</h2>
+
+        {/*LINKS*/}
         <ul>
           <li>
             <a href="http://localhost:3000/">Modulo di contatto</a>
@@ -15,6 +17,8 @@ function Sidebar() {
             <a href="http://localhost:3000/">Stylish table</a>
           </li>
         </ul>
+        {/*END LINKS*/}
+
       </div>
       <div className="sidebar-footer">
         <h5>&lt;/&gt; by Pasquale</h5>
